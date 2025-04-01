8ab419933aa565f3a4ad4c7a31d38d87f2f48253
PatrickAgeev <patrick_ag@mail.ru>
2025-04-01 11:40:49 +0300

    Update DataGridColumn.cs
    binabled With Property for Dynamic Resize Width Column

--- src/UraniumUI.Material/Controls/DataGridColumn.cs
+++ src/UraniumUI.Material/Controls/DataGridColumn.cs
@@ -27,8 +27,25 @@ public class DataGridColumn : BindableObject
 
     public BindingBase ValueBinding { get; set; }
 
-    [TypeConverter(typeof(GridLengthTypeConverter))]
-    public GridLength Width { get; set; } = GridLength.Auto;
+   public GridLength Width
+	{
+		get => (GridLength) GetValue(WidthProperty); set => SetValue(WidthProperty, value);
+	}
+
+	[TypeConverter(typeof(GridLengthTypeConverter))]
+	public static readonly BindableProperty WidthProperty = BindableProperty.Create(
+		nameof(Width),
+		typeof(GridLength),
+		typeof(DataGridColumn),
+		GridLength.Auto,
+		propertyChanged: (bindable, oldValue, newValue) =>
+		{
+        if (bindable is DataGridColumn column)
+        {
+        	column.OnPropertyChanged(nameof(Width));
+        }
+});
+  
 
     public bool IsVisible { get => (bool)GetValue(IsVisibleProperty); set => SetValue(IsVisibleProperty, value); }
 
